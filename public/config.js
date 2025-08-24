17dc9e2a7d2866c7c182661013208790017188c3
Propoc <mert.ali.nemli@hotmail.com>
2025-08-25 01:51:46 +0300

    httsp

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SERVER_URL: 'http://master.d1zvkss672qhs2.amplifyapp.com/'  //http for local
+  SERVER_URL: 'https://master.d1zvkss672qhs2.amplifyapp.com/'  //http for local
 };
 
 //git log --oneline 
