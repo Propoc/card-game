dd8dc4df3ac9d6dfc78a89c6665bfe269433bfdb
Propoc <mert.ali.nemli@hotmail.com>
2025-08-24 23:37:10 +0300

    xxx

--- src/socket.js
+++ src/socket.js
@@ -1,91 +1,36 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = window.APP_CONFIG?.SERVER_URL || process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+
+
+const SERVER_URL = window.APP_CONFIG?.SERVER_URL;
+console.log('[Socket] SERVER_URL:', SERVER_URL);
 
 // Create a socket wrapper that can be updated
-class SocketWrapper {
-  constructor() {
-    this.socket = null;
-    this.listeners = new Map();
-    this.init();
+
+import { io } from "socket.io-client";
+
+
+const socket = io(SERVER_URL, {
+  autoConnect: true,
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
+});
+
+socket.on('connect', () => {
+  console.log('[Socket] Connected:', socket.id);
+});
+socket.on('connect_error', (err) => {
+  console.error('[Socket] Connection error:', err);
+  if (err && err.message) {
+    console.error('[Socket] Error message:', err.message);
   }
-
-  init() {
-    this.socket = io(SERVER_URL, {
-      autoConnect: true,
-      timeout: 1000,
-    });
-
-    this.setupFallback();
-    this.reattachListeners();
+  if (err && err.stack) {
+    console.error('[Socket] Error stack:', err.stack);
   }
+});
+socket.on('disconnect', (reason) => {
+  console.warn('[Socket] Disconnected:', reason);
+});
 
-  setupFallback() {
-    this.socket.on('connect_error', () => {
-      if (!SERVER_URL.includes('localhost')) {
-        console.log('Connection failed to:', SERVER_URL);
-        console.log('Attempting fallback to localhost...');
-        
-        // Clean up old socket
-        this.socket.off();
-        this.socket.disconnect();
-
-        // Create new socket with localhost
-        this.socket = io('http://localhost:4000', { autoConnect: true });
-        this.reattachListeners();
-      }
-    });
-  }
-
-  reattachListeners() {
-    // Reattach all stored listeners to the new socket
-    for (const [event, handlers] of this.listeners) {
-      for (const handler of handlers) {
-        this.socket.on(event, handler);
-      }
-    }
-  }
-
-  on(event, handler) {
-    // Store the listener for reattachment
-    if (!this.listeners.has(event)) {
-      this.listeners.set(event, new Set());
-    }
-    this.listeners.get(event).add(handler);
-    
-    // Attach to current socket
-    this.socket.on(event, handler);
-  }
-
-  off(event, handler) {
-    if (handler) {
-      // Remove specific handler
-      if (this.listeners.has(event)) {
-        this.listeners.get(event).delete(handler);
-      }
-      this.socket.off(event, handler);
-    } else {
-      // Remove all handlers for event
-      this.listeners.delete(event);
-      this.socket.off(event);
-    }
-  }
-
-  emit(...args) {
-    return this.socket.emit(...args);
-  }
-
-  disconnect() {
-    this.socket.disconnect();
-  }
-
-  get id() {
-    return this.socket.id;
-  }
-
-  get connected() {
-    return this.socket.connected;
-  }
-}
-
-export const socket = new SocketWrapper();
+export { socket };
