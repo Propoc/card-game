f79e7cbf081c3f73e001df713185515471ca659a
Propoc <mert.ali.nemli@hotmail.com>
2025-08-25 13:39:01 +0300

    withssl

--- public/config.js
+++ public/config.js
@@ -1,7 +1,12 @@
 window.APP_CONFIG = {
-  SERVER_URL: 'http://ec2-3-75-199-141.eu-central-1.compute.amazonaws.com:4000'  //http for local
+  SERVER_URL: 'https://api.www.nemli.site/'  //http for local
 };
 
 //git log --oneline 
 //git reset --hard xd
 //gt push --force
+
+
+//ssh -i .\cardpair.pem ubuntu@63.179.188.199
+//cd card-game/server
+//node server.js
