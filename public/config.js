83bec4976c6fe2d8f98007d341141347b48a3aa8
Propoc <mert.ali.nemli@hotmail.com>
2025-08-30 22:10:58 +0300

    help-ready

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  //SERVER_URL: 'https://api.nemli.site/'
-  SERVER_URL: 'localhost:4000'
+  SERVER_URL: 'https://api.nemli.site/'
+  //SERVER_URL: 'localhost:4000'
 };
 
 //git log --oneline 
